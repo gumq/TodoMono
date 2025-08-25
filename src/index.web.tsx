c0c423103b55bee62637c4813619656b03053675
gum <tranquyen140420@gmail.com>
2025-08-25 10:58:17 +0700

    Updates

---
+++ src/index.web.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import {AppRegistry} from 'react-native';
+import App from '../../App';
+import {name as appName} from '../../App';
+
+// Đăng ký component App
+AppRegistry.registerComponent(appName, () => App);
+
+// Render ra web
+console.log('index.web.tsx is running');
+AppRegistry.runApplication(appName, {
+  rootTag: document.getElementById('root'),
+});
---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+/* eslint-disable prettier/prettier */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const appDirectory = path.resolve(__dirname, '..');
+const isProd = process.env.NODE_ENV === 'production';
+module.exports = {
+  mode: isProd ? 'production' : 'development',
+  entry: path.resolve(appDirectory, 'web/index.web.tsx'),
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(appDirectory, 'dist/web'),
+    clean: true,
+    publicPath: isProd ? '/' : '/',
+  },
+  resolve: {
+    extensions: [
+      '.web.tsx',
+      '.web.ts',
+      '.web.js',
+      '.tsx',
+      '.ts',
+      '.js',
+      '.json',
+    ],
+    alias: {
+      'react-native$': 'react-native-web',
+      'react-native-reanimated': path.resolve(
+        __dirname,
+        'web/fake-reanimated.js',
+      ),
+      '@react-native-firebase/messaging': path.resolve(
+        __dirname,
+        'web/mocks/empty.js',
+      ),
+      'react-native-push-notification': path.resolve(
+        __dirname,
+        'web/mocks/empty.js',
+      ),
+      'react-dom/client': 'react-dom',
+      'react-native-fs': path.resolve(__dirname, 'web/mocks/empty.js'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        include: [
+          path.resolve(appDirectory, 'App.tsx'),
+          path.resolve(appDirectory, 'src'),
+          path.resolve(appDirectory, 'web'),
+          path.resolve(appDirectory, 'node_modules/react-native-vector-icons'),
+          // thêm các node_modules cần transpile nếu cần
+        ],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            presets: [
+              'module:metro-react-native-babel-preset',
+              '@babel/preset-typescript',
+              '@babel/preset-react',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    port: 8080,
+    historyApiFallback: true,
+    static: {
+      directory: path.resolve(appDirectory, 'public'), // nếu có
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(appDirectory, 'web/index.html'),
+    }),
+  ],
+};
